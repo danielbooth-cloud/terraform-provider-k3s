69e61130bd26eb12db1836bdefb3dd5aeb123850
Daniel Booth <user@danielbooth.cloud>
2025-08-17 11:35:05 +0100

    feat: increment provider version

--- main.go
+++ main.go
@@ -9,13 +9,15 @@ import (
 	"striveworks.us/terraform-provider-k3s/internal/provider"
 )
 
+var version string = "0.2.3"
+
 func main() {
 	var debug bool
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	err := providerserver.Serve(context.Background(), provider.New(""), providerserver.ServeOpts{
+	err := providerserver.Serve(context.Background(), provider.New(version), providerserver.ServeOpts{
 		Address: "registry.opentofu.org/striveworks/k3s",
 		Debug:   debug,
 	})
